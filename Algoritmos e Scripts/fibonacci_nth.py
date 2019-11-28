6dd6f59bba5799610c968bde2a13f914d246a520
Gabriel Felippe <gabrielfelippe90@gmail.com>
2019-11-28 07:09:42 -0300

    Create fibonacci_nth.py

---
+++ Algoritmos e Scripts/fibonacci_nth.py
@@ -0,0 +1,21 @@
+# Dada uma posição fornecida pelo usuário, calcule o número Fibonacci referente e ela
+# 1 -> 0
+# 2 -> 1
+# 3 -> 1
+# 4 -> 2 
+# 5 -> 3
+# 6 -> 5
+
+def fibonacci_nth(n, calculado={1:0, 2:1, 3:1}):
+	if n in calculado:
+		return calculado[n]
+
+	calculado[n] = fibonacci_nth(n-1, calculado) + fibonacci_nth(n-2, calculado)
+	return calculado[n]
+
+print(fibonacci_nth(1))
+print(fibonacci_nth(2))
+print(fibonacci_nth(3))
+print(fibonacci_nth(4))
+print(fibonacci_nth(5))
+print(fibonacci_nth(6))
