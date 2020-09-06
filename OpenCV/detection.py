55131a592372ae2adf52bcaaff3d9ae58e4254b6
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-09-06 00:39:17 -0300

    Exemplo de Detecção Facial adicionado

---
+++ OpenCV/detection.py
@@ -0,0 +1,14 @@
+import cv2
+
+GREEN = (0,255,0)
+
+faces = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+image = cv2.imread("físicos.jpg")
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+detections = faces.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=6)
+
+for (x, y, w, h) in detections:
+	cv2.rectangle(image, (x,y), (x+w,y+h), GREEN, 2)
+
+cv2.imshow("output", image)
+cv2.waitKey(0)
