76620d2efff3b547b81a0d925bd4c23150ff64ad
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-01-30 13:01:38 -0300

    Cores atualizadas

--- turtle/tutorial.py
+++ turtle/tutorial.py
@@ -6,30 +6,24 @@ turtle.title('Meu Desenho')
 
 # Abrindo a tela de desenhos
 screen = turtle.Screen()
-screen.bgcolor('#967ee0')
+screen.bgcolor('#6b1ac7')
 screen.setup(650,650)
 
 # Inicializando e configurando a tartaruga
 tartaruga = turtle.Turtle()
 tartaruga.shape('turtle') 
-tartaruga.color('purple') 
+tartaruga.color('#00ffee') 
 tartaruga.shapesize(2,2,2)
 tartaruga.speed(1)
-tartaruga.pen(fillcolor="orange", pensize=4)
+tartaruga.pen(fillcolor="#2b00ff", pensize=4)
 
-# Desenhando o primeiro círculo
-tartaruga.begin_fill()
-tartaruga.circle(150)
-tartaruga.end_fill()
-tartaruga.hideturtle()
+for i in range(150,30,-20):
+	tartaruga.begin_fill()
+	tartaruga.circle(i)
+	tartaruga.end_fill()
+	tartaruga.hideturtle()
 
-# Desenhando o segundo círculo
-tartaruga.begin_fill()
-tartaruga.circle(120)
-tartaruga.end_fill()
-tartaruga.hideturtle()
 tartaruga.dot(35)
-
 # Transformando a tartaruga em um círculo
 tartaruga.shape('circle')
 tartaruga.showturtle()
