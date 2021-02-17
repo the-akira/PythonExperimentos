7f32c642cb40985e291e5ddfe596f7357a57f3b2
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-02-17 05:01:51 -0300

    Programa que Converte um Inteiro para uma String Romano

---
+++ Números Romanos/int_to_roman.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+
+def write_roman(num):
+    roman = OrderedDict()
+    roman[1000] = "M"
+    roman[900] = "CM"
+    roman[500] = "D"
+    roman[400] = "CD"
+    roman[100] = "C"
+    roman[90] = "XC"
+    roman[50] = "L"
+    roman[40] = "XL"
+    roman[10] = "X"
+    roman[9] = "IX"
+    roman[5] = "V"
+    roman[4] = "IV"
+    roman[1] = "I"
+
+    def roman_num(num):
+        for r in roman.keys():
+            x, y = divmod(num, r)
+            yield roman[r] * x
+            num -= (r * x)
+            if num <= 0:
+                break
+
+    return "".join([a for a in roman_num(num)])
+
+print(write_roman(1))
+print(write_roman(50))
+print(write_roman(3))
+print(write_roman(7))
