49b55e2848e599f71b0a50ad34b18d34fb891d03
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-07 00:17:06 -0300

    Logging atualizado

--- logging/log_teste.py
+++ logging/log_teste.py
@@ -12,7 +12,6 @@ import logging
 
 logging.basicConfig(filename='test.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
-
 def add(x, y):
     """Add Function"""
     return x + y
@@ -32,18 +31,17 @@ def divide(x, y):
     """Divide Function"""
     return x / y
 
+n1 = 47
+n2 = 10.55
 
-num_1 = 47
-num_2 = 10.55
+add_result = add(n1, n2)
+logging.debug('Add: {} + {} = {}'.format(n1, n2, add_result))
 
-add_result = add(num_1, num_2)
-logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+sub_result = subtract(n1, n2)
+logging.debug('Sub: {} - {} = {}'.format(n1, n2, sub_result))
 
-sub_result = subtract(num_1, num_2)
-logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+mul_result = multiply(n1, n2)
+logging.debug('Mul: {} * {} = {}'.format(n1, n2, mul_result))
 
-mul_result = multiply(num_1, num_2)
-logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
-
-div_result = divide(num_1, num_2)
-logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+div_result = divide(n1, n2)
+logging.debug('Div: {} / {} = {}'.format(n1, n2, div_result))
