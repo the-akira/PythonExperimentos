36781145c664aa24b759dde211c74684172b5bea
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-07 13:22:20 -0300

    Experimentos com OpenCV atualizados

--- OpenCV/detection.py
+++ OpenCV/detection.py
@@ -8,7 +8,7 @@ image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 detections = faces.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=6)
 
 for (x, y, w, h) in detections:
-	cv2.rectangle(image, (x,y), (x+w,y+h), GREEN, 2)
+    cv2.rectangle(image, (x,y), (x+w,y+h), GREEN, 2)
 
 cv2.imshow("output", image)
 cv2.waitKey(0)
