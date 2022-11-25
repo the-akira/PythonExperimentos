9523519d05e2b93aadb3b6313dca91eaeb9d35c6
Gabriel Felippe <gabrielfelippe@outlook.com>
2022-11-25 19:18:02 -0300

    Corrigida Indentação

--- Algoritmos/Stack Data Structure/evaluate_reverse_polish_notation.py
+++ Algoritmos/Stack Data Structure/evaluate_reverse_polish_notation.py
@@ -1,23 +1,23 @@
 import operator
 
 def eval_reverse_polish_notation(tokens):
-	stack = []
+    stack = []
 
-	ops = {
-		"+": operator.add,
-		"-": operator.sub,
-		"*": operator.mul,
-		"/": operator.truediv
-	}
+    ops = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv
+    }
 
-	for token in tokens:
-		if token not in ops:
-			stack.append(int(token))
-		else:
-			n2 = stack.pop()
-			n1 = stack.pop()
-			result = ops[token](n1,n2)
-			stack.append(int(result))
-	return stack[0]
+    for token in tokens:
+        if token not in ops:
+            stack.append(int(token))
+        else:
+            n2 = stack.pop()
+            n1 = stack.pop()
+            result = ops[token](n1,n2)
+            stack.append(int(result))
+    return stack[0]
 
 print(eval_reverse_polish_notation("01-33*7/+"))
